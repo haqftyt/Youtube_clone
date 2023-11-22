fae97ed4a847a518c4398631809df17826ca7862
haqftyt <fhaq4701@gmail.com>
2023-11-22 17:28:45 +0530

    youtube clone created

---
+++ Html_Forms/practice.js
@@ -0,0 +1,20 @@
+function Realme(){
+    console.log("My phone is Realme 5i")
+}
+function add(){
+    var a=10;
+    b=20;
+    c=a+b;
+    console.log("C="+c)
+}
+function favourite(){
+    factor="Joseph vijay";
+    var fplayer="dhoni";
+    var fmovie="Leo";
+    console.log("Favourite Actor:"+factor)
+    console.log("Favourite player:"+fplayer)
+    console.log("Favourite movie:"+fmovie)
+}
+
+favourite()
+
